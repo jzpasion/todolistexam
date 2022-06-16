b11fa11df51c38e7399bc636c09b95d575e5566b
unknown <zyrel05pasion1105@gmail.com>
2022-06-16 21:44:42 +0800

    onClick function on finished task DONE

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState} from "react";
 import Todolist from "./pages/Todolist";
 import './App.css';
 import { ListGroup,Form,InputGroup } from "react-bootstrap";
+import Swal from "sweetalert2";
 
 const App=()=> {
   let itemListInitial = [ 
@@ -15,14 +16,50 @@ const App=()=> {
   let [itemList , setItemList] = useState (itemListInitial);
   let [result , setResult] = useState("");
 
+  const onItemClick =(index)=>{
+    //Add new task
+      let tempList = [...itemList];
+      tempList[index].status = false;
+      setItemList(tempList);
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: tempList[index].taskName+ ' Task Done!',
+        showConfirmButton: false,
+        timer: 1500
+      })
+   
+  }
+
+  const onEnter =(e)=>{
+    if (e.key === 'Enter') {
+      //check if input is not null
+      if(e.target.value !== " " || e.target.value !== ""){
+        let checkList = [...itemList];
+        var checker = checkList.find(element =>(
+          element.taskName.toLocaleLowerCase() === e.target.value.toLocaleLowerCase().trim()
+          
+        ))
+        if(checker === undefined){
+          let templist = [{taskName: e.target.value , status: true } , ...itemList]
+          setItemList(templist);
+          setResult("");
+        }else{
+          alert("Task name is already on the list!");
+          setResult("");
+        }
+      }
+    }
+  }
+
   return (
     <div className='container'>
     <InputGroup>
-      <Form.Control className="number-input" onChange={(e)=>{setResult(e.target.value)}} value={result} />
+      <Form.Control className="number-input" onKeyDown={onEnter} onChange={(e)=>{setResult(e.target.value)}} value={result} />
     </InputGroup>
 
     <ListGroup>
-      <Todolist itemList={itemList}/>
+      <Todolist itemList={itemList} clickFunction={onItemClick}/>
     </ListGroup>
 </div>
   );
--- src/pages/Todolist.js
+++ src/pages/Todolist.js
@@ -1,13 +1,13 @@
 import { React } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 
-const Todolist = ({itemList}) =>{
+const Todolist = ({itemList,clickFunction}) =>{
     
     return(
         <ListGroup >
             {itemList.map((list , index) =>(
-                list.status? <ListGroup.Item key={index} className='listItems' >{list.taskName}</ListGroup.Item> 
-                :<ListGroup.Item key={index} className='disabledItems'>{list.taskName}</ListGroup.Item>
+                list.status? <ListGroup.Item key={index} className='listItems'  onClick={()=>clickFunction(index)}>{list.taskName}</ListGroup.Item> 
+                :<ListGroup.Item key={index} className='disabledItems' >{list.taskName}</ListGroup.Item>
             ))}
         </ListGroup>
     );
